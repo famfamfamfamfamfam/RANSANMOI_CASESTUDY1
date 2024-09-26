cf052eba34ba581bbf5a72180d8f698fa17dd63c
BinhTran <fam.out16@gmail.com>
2024-09-27 06:16:01 +0700

    Snake Game

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RANSANMOI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread _game = new Thread(SnakeControl.ListenKey);
+            _game.Start();
+            do
+            {
+                Console.Clear();
+                SnakeControl.Drawboard();
+                SnakeControl.setUpBoard();
+                SnakeControl.MoveSnakeHead();
+                SnakeControl.EatFood();
+                SnakeControl.SpawnBody();
+                SnakeControl.PopUpfood();
+                SnakeControl.ShowPoint();
+                SnakeControl.ExitGame();
+                //Task.Delay(SnakeControl.speed).Wait();
+                Thread.Sleep(SnakeControl.speed);
+            }
+            while (true);
+        }
+    }
+}
